122f62b22325468fc91a68231c805877ea884cf7
Adam Bouck <Adam.j.Bouck@gmail.com>
2013-10-17 21:12:07 -0700

    Yo Angular

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('clastatApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
